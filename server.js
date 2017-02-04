22bea7e6d9a15c143f633efdb01efb053ccf3bda
Harry A. Swan <has.land@hotmail.co.uk>
2017-02-04 22:35:25 +0000

    initial starting point

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.redirect('/game');
+});
+
+var gameRouter = express.Router();
+gameRouter.get('/', fuction(req, res) {
+	res.send('main route for game');
+});
+gameRouter.get('/play', function(req, res) {
+	res.send('play route');
+});
+gameRouter.get('/play/:id/:id2', function(req, res) {
+	res.send('play with id1: ' + req.params.id + ' & id2: ' + req.params.id2);
+}
+
+app.use('/game', gameRouter);
+
+
+
+
+
+
+
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
